09acf8ad7d7b02464093d08b3e5efafda28ca7ee
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-21 11:58:14 +0000

    Removed duplicate code using .map

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
+import UUIDV4 from "./utilities/UUIDV4";
+
 const App = () => {
+
+  const categories = [
+    {
+      id: UUIDV4(),
+      title: "Hats"
+    },
+    {
+      id: UUIDV4(),
+      title: "Jackets"
+    },
+    {
+      id: UUIDV4(),
+      title: "Sneakers"
+    },
+    {
+      id: UUIDV4(),
+      title: "Womens"
+    },
+    {
+      id: UUIDV4(),
+      title: "Mens"
+    }
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {
+        categories.map(category => {
+          return (
+            <div className="category-container" key={category.id}>
+              <div className="category-body-container">
+                <h2>{category.title}</h2>
+                <p>Shop Now</p>
+              </div>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
