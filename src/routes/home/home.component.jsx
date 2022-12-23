e4291da3db0e75fbacdbb2e739ffb913184781f4
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-23 15:59:33 +0000

    Extracting Home component.

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,39 @@
+import UUIDV4 from "./utilities/UUIDV4";
+import Categories from "../../components/categories/categories.component";
+
+const Home = () => {
+
+  const categories = [
+    {
+      id: UUIDV4(),
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id: UUIDV4(),
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id: UUIDV4(),
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id: UUIDV4(),
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      id: UUIDV4(),
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+
+  return (
+    <Categories categories={categories} />
+  );
+}
+
+export default Home;
