07a990286a007c2cc3b6af0d98f0648504c89c32
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-24 10:06:55 +0000

    Logo created.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import "./navigation.styles.scss"; 
+
 const Navigation = () => {
     return (
         <Fragment>
