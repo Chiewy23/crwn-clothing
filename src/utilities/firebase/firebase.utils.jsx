4501510ab5cc1b76c9d34e9831ff5260dbf6a71c
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-25 13:01:13 +0000

    Sign-in with Google feature added.

--- src/utilities/firebase/firebase.utils.jsx
+++ src/utilities/firebase/firebase.utils.jsx
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDfSuWd5WhxPAs1SONWhekzBsBXiaVtXVA",
@@ -7,9 +8,16 @@ const firebaseConfig = {
     storageBucket: "crwn-clothing-db-61eec.appspot.com",
     messagingSenderId: "249324018099",
     appId: "1:249324018099:web:229eae4960de57eb3553c6"
-  };
+};
   
   
-  // Initialize Firebase
-  
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    propmt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
