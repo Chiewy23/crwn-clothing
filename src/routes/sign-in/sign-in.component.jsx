3ada42d8eaae5e74c2b988f7fe125f5f57d670ff
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-25 13:05:15 +0000

    Sign-In method implemented.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopup } from "../../utilities/firebase/firebase.utils";
+
 const SignIn = () => {
+    const loginGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+
+    }
+
     return (
         <div>
             <h1>Sign In</h1>
+            <button onClick={loginGoogleUser}>Sign in with Google Popup</button>
         </div>
     )
 }
