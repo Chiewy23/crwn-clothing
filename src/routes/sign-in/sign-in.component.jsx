ff77789c485a16464ca8dc6806ad57ff161090a7
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-27 09:10:42 +0000

    Implemented redirect sign-in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,16 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utilities/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from "../../utilities/firebase/firebase.utils";
+import { getRedirectResult } from "firebase/auth";
+import { useEffect } from "react";
 
 const SignIn = () => {
+
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);
+
     const loginGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -10,6 +20,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In</h1>
             <button onClick={loginGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 }
