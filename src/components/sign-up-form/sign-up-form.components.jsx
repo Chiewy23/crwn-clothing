09d762f7371036697885d9d87097caf2ec8727dc
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-27 09:22:13 +0000

    Fields created.

--- src/components/sign-up-form/sign-up-form.components.jsx
+++ src/components/sign-up-form/sign-up-form.components.jsx
@@ -1,7 +1,20 @@
 const SignUpForm = () => {
     return (
         <div>
-            
+            <h1>Sign Up With Your Email & Password</h1>
+            <form>
+                <label>Display Name</label>
+                <input required/>
+
+                <label>Email</label>
+                <input required/>
+
+                <label>Password</label>
+                <input required/>
+
+                <label>Password</label>
+                <input required/>
+            </form>
         </div>
     );
 }
