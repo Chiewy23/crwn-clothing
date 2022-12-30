eb0ad61c18a29bc28481474272172afb55774c1e
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-30 15:39:53 +0000

    Removed redundant sections.

--- src/components/sign-in-form/sign-in-form.components.jsx
+++ src/components/sign-in-form/sign-in-form.components.jsx
@@ -7,15 +7,13 @@ import Button from "../button/button.component";
 import "./sign-in-form.styles.scss";
 
 const defaultFormFields = {
-    displayName: "",
     email: "",
-    password: "",
-    confirmPassword: ""
+    password: ""
 };
 
 const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
-    const { displayName, email, password, confirmPassword } = formFields;
+    const { email, password } = formFields;
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -25,24 +23,11 @@ const SignInForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (password !== confirmPassword) {
-            alert("Passwords do not match!");
-            return;
-        }
-
         try {
-
-            const { user } = await createAuthUserWithEmailAndPassword(email, password);
-            await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
 
         } catch(error) {
-            
-            if (error.code === "auth/email-already-in-use") {
-                alert("Cannot create user - email already in use!");
-            } else {
-                console.log(`ERROR: User creation encountered an error. Please try again - ${error}`);
-            }
+
         }
     }
 
@@ -52,13 +37,11 @@ const SignInForm = () => {
 
     return (
         <div className="sign-up-container">
-            <h2>Don't have an account?</h2>
-            <span>Sign up with your email & password</span>
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email & password</span>
             <form onSubmit={handleSubmit}>
-                <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
                 <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} />
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
-                <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
                 <Button type="submit">Submit</Button>
             </form>
         </div>
