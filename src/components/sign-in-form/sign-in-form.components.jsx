98906d43a8285fd82b517cc4b454c7e24d528219
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-31 08:07:25 +0000

    Minor bug fix.
    Removing default submit behaviour of Google button.

--- src/components/sign-in-form/sign-in-form.components.jsx
+++ src/components/sign-in-form/sign-in-form.components.jsx
@@ -62,7 +62,7 @@ const SignInForm = () => {
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button buttonType="google" onClick={signInWithGoogle}>Google Sign In</Button>
+                    <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google Sign In</Button>
                 </div>
             </form>
         </div>
