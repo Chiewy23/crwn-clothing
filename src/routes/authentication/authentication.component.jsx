1a174751e6850002db05bae7fbfef169a70354ff
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-31 08:14:35 +0000

    Styling applied to sign-in page.

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,28 +1,11 @@
-import { createUserDocumentFromAuth, auth } from "../../utilities/firebase/firebase.utils";
-import { getRedirectResult } from "firebase/auth";
-import { useEffect } from "react";
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
 import SignInForm from "../../components/sign-in-form/sign-in-form.components";
 
+import "./authentication.styles.scss";
+
 const Authentication = () => {
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await getRedirectResult(auth);
-            if (response) {
-                return await createUserDocumentFromAuth(response.user);
-            } else {
-                console.log("User not signed-in");
-            }
-        }
-
-        fetchData();
-    }, []);
-
     return (
-        <div>
-            <h1>Sign In</h1>
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm />
         </div>
