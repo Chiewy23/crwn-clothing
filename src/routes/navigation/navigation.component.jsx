d54bbc47d44671dfaffbeed6942a41832a357fe3
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-01 17:09:35 +0000

    Removed redundant code.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,8 +7,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
-    console.log("Success");
-    console.log(currentUser);
+    
     return (
         <Fragment>
             <div className="navigation">
