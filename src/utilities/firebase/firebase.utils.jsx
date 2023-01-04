609d930f759e5277742ea2e0bb2e338447738111
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-04 14:43:37 +0000

    Code tidy-up: Firebase utils.

--- src/utilities/firebase/firebase.utils.jsx
+++ src/utilities/firebase/firebase.utils.jsx
@@ -37,9 +37,10 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const db = getFirestore();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
 
