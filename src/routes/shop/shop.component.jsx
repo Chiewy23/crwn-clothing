f4282f8b4504b5845b8fb794b0200bb7484c443e
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-07 14:48:43 +0000

    Styling added to Product Card.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,11 +3,13 @@ import { ProductsContext } from "../../contexts/products.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
+import "./shop.styles.scss";
+
 const Shop = () => {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div>
+        <div className="products-container">
             {products.map((product) => {
                 return <ProductCard key={product.id} product={product} />
             })};
