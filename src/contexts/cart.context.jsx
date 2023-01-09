caca519d3f955177dd220085b1d6ff3209e1ff03
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-09 15:49:18 +0000

    Handler for adding items to cart.

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,12 +2,33 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 });
+
+const addCartItem = (cartItems, itemToAdd) => {
+    const existingItem = cartItems.find(item => item.id === itemToAdd.id);
+
+    if (existingItem) {
+        return cartItems.map(item => {
+            return item.id === itemToAdd.id ? { ...item, quantity: item.quantity++ } : item;
+        })
+    }
+
+    return [...cartItems, { ...itemToAdd, quantity: 1 }]
+}
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen}
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (itemToAdd) => {
+        setCartItems(addCartItem(cartItems, itemToAdd));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems}
+
     return (
         <CartContext.Provider value={value}>{ children }</CartContext.Provider>
     );
