95865e04b52cb657e7b07f460237e8b23073b3c8
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-12 15:36:35 +0000

    Listing items in checkout page.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,23 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
-const Checkout = ({ items }) => {
+import CartItem from "../../components/cart-item/cart-item.component";
+
+const Checkout = () => {
+    const { cartItems } = useContext(CartContext);
 
     return (
-        <div>Checkout</div>
+        <div>
+            <div className='cart-items'>
+                {cartItems.length ? (
+                cartItems.map((cartItem) => (
+                    <CartItem key={cartItem.id} cartItem={cartItem} />
+                ))
+                ) : (
+                <span className='empty-message'>Your cart is empty</span>
+                )}
+            </div>
+        </div>
     )
 };
 
