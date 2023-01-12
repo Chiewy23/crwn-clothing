360e87a2f5288b860ed1d6ba4959ecea985d7ffb
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-12 15:48:22 +0000

    Initial start to checkout behaviour.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,23 +1,28 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-import CartItem from "../../components/cart-item/cart-item.component";
-
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, addItemToCart } = useContext(CartContext);
 
     return (
         <div>
-            <div className='cart-items'>
-                {cartItems.length ? (
-                cartItems.map((cartItem) => (
-                    <CartItem key={cartItem.id} cartItem={cartItem} />
-                ))
-                ) : (
-                <span className='empty-message'>Your cart is empty</span>
-                )}
+            <h1>Checkout Page</h1>
+            <div>
+                {
+                    cartItems.map(cartItem => {
+                        const {id, name, quantity } = cartItem;
+                        return (
+                            <div key={ id }>
+                                <h2>{ name }</h2>
+                                <span>{ quantity }</span>
+                                <span>decrement</span>
+                                <span onClick={() => addItemToCart(cartItem)}>increment</span>
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
     )
