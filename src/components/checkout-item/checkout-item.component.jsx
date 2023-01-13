0d4cc20e5550cd466f75f9d9bfe54941df3042bd
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-13 15:49:14 +0000

    Increment and decrement handlers created.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -8,6 +8,8 @@ const CheckoutItem = ({ cartItem }) => {
     const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
 
     const clearItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemFromCart(cartItem);
 
     return (
         <div className="checkout-item-container">
@@ -15,7 +17,11 @@ const CheckoutItem = ({ cartItem }) => {
                 <img  src={imageUrl} alt={name}/>
             </div>
             <span className="name">{ name }</span>
-            <span className="quantity">{ quantity }</span>
+            <span className="quantity">
+                <div className="arrow" onClick={removeItemHandler}>&#10094;</div>
+                <span className="value">{ quantity }</span>
+                <div className="arrow" onClick={addItemHandler}>&#10095;</div>
+            </span>
             <span className="price">{ price }</span>
             <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
         </div>
