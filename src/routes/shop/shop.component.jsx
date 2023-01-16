4ed70b6c294538b495bbe60960c6d16547e987db
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-16 15:48:21 +0000

    WIP Using categories.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,18 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
+// import { useContext } from "react";
+// import { CategoriesContext } from "../../contexts/categories.context";
 
-import ProductCard from "../../components/product-card/product-card.component";
+// import ProductCard from "../../components/product-card/product-card.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext);
+    // const { categoriesMap } = useContext(CategoriesContext);
 
     return (
         <div className="products-container">
-            {products.map((product) => {
-                return <ProductCard key={product.id} product={product} />
-            })}
+            {/* {categoriesMap.map((category) => {
+                return <ProductCard key={category.id} product={category} />
+            })} */}
         </div>
     )
 };
