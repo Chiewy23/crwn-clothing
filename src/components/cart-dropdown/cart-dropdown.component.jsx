fcb5d17252ced969d618a75e5eed28bc7ee72769
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-21 19:15:33 +0000

    Initial start to Cart Dropdown styled component.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
-import "./cart-dropdown.styles.scss";
+import "./cart-dropdown.styles";
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -18,13 +18,15 @@ const CartDropdown = () => {
     return (
       <div className='cart-dropdown-container'>
         <div className='cart-items'>
-          {cartItems.length ? (
-            cartItems.map((cartItem) => (
-              <CartItem key={cartItem.id} cartItem={cartItem} />
-            ))
-          ) : (
-            <span className='empty-message'>Your cart is empty</span>
-          )}
+          {
+            cartItems.length ? (
+              cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} cartItem={cartItem} />
+              ))
+            ) : (
+              <span className='empty-message'>Your cart is empty</span>
+            )
+          }
         </div>
         
         <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
