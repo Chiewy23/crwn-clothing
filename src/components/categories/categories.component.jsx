fdfca2adc24c1b9bf1568637fcf1023340d42a8b
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-24 15:13:43 +0000

    Moving json data into Categories component.

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -1,8 +1,42 @@
 import DirectoryItem from "./directory-item/directory-item.component";
+import UUIDV4 from "../../utilities/UUIDV4.utils";
 
 import "./categories.styles.scss";
 
-const Categories = ({categories}) => {
+const categories = [
+  {
+    id: UUIDV4(),
+    title: "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats"
+  },
+  {
+    id: UUIDV4(),
+    title: "Jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets"
+  },
+  {
+    id: UUIDV4(),
+    title: "Sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers"
+  },
+  {
+    id: UUIDV4(),
+    title: "Womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens"
+  },
+  {
+    id: UUIDV4(),
+    title: "Mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens"
+  }
+];
+
+const Categories = () => {
     return (
         <div className="categories-container">
         {
