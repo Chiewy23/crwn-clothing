7eba9d540c496dd86e2fff2babe57b1f3bfa792c
Christopher Hiew <christopherhiew@protonmail.com>
2023-01-26 15:33:13 +0000

    Converted CheckoutItem to styled component.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,13 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
-import "./checkout-item.styles.scss";
+import { 
+    CheckoutItemContainer,
+    ImageContainer,
+    Attribute,
+    Quantity,
+    RemoveButton
+} from "./checkout-item.styles";
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
@@ -12,19 +18,19 @@ const CheckoutItem = ({ cartItem }) => {
     const removeItemHandler = () => removeItemFromCart(cartItem);
 
     return (
-        <div className="checkout-item-container">
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img  src={imageUrl} alt={name}/>
-            </div>
-            <span className="name">{ name }</span>
-            <span className="quantity">
+            </ImageContainer>
+            <Attribute>{ name }</Attribute>
+            <Quantity>
                 <div className="arrow" onClick={removeItemHandler}>&#10094;</div>
                 <span className="value">{ quantity }</span>
                 <div className="arrow" onClick={addItemHandler}>&#10095;</div>
-            </span>
-            <span className="price">{ price }</span>
-            <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
-        </div>
+            </Quantity>
+            <Attribute>{ price }</Attribute>
+            <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
     );
 };
 
